5bd2b0bd06a7d677555cb3ba6d7ba26ce3356b71
philip120 <109464319+philip120@users.noreply.github.com>
2024-12-01 11:52:48 +0200

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .video_utils import read_video, save_video, read_video_in_chunks, save_video_incremental, get_video_fps
-from .drawing_utils import get_center_of_bbox, calculate_speed
-from .lane_detections import detect_lane_ropes_yolo, load_yolov8_model
